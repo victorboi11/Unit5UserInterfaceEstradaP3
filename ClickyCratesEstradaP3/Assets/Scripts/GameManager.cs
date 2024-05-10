d8ecdbaa786f3737574d3474f7e165b3a802556c
victorboi11 <725304@student.dusd.net>
2024-05-10 12:19:56 -0700

    ergerg
    ergergterg

--- ClickyCratesEstradaP3/Assets/Scripts/GameManager.cs
+++ ClickyCratesEstradaP3/Assets/Scripts/GameManager.cs
@@ -2,22 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
 
 public class GameManager : MonoBehaviour
 {
     public List<GameObject> targets;
     public TextMeshProUGUI scoreText;
     public TextMeshProUGUI gameOverText;
+    public Button restartButton;
+    public GameObject titleScreen;
     public bool isGameActive;
     private int score;
     private float spawnRate = 1.0f;
+   
     // Start is called before the first frame update
     void Start()
     {
-        isGameActive = true;
-        StartCoroutine(SpawnTarget());
-        score = 0;
-        UpdateScore(0);
+     
     }
 
     // Update is called once per frame
@@ -44,8 +47,28 @@ public class GameManager : MonoBehaviour
 
     public void GameOver()
     {
+        restartButton.gameObject.SetActive(true);
         gameOverText.gameObject.SetActive(true);
         isGameActive = false;
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+
+    public void StartGame()
+    {
+        isGameActive = true;
+        score = 0;
+
+        StartCoroutine(SpawnTarget());             
+        UpdateScore(0);
+
+        titleScreen.gameObject.SetActive(false);
+        
+       
     }
 
 }
--- ClickyCratesEstradaP3/Assets/Scripts/Target.cs
+++ ClickyCratesEstradaP3/Assets/Scripts/Target.cs
@@ -45,7 +45,7 @@ public class Target : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         Destroy(gameObject);
-        if(!gameObject.CompareTag("Bad"))
+        if(!gameObject.CompareTag("Bad") && gameManager.isGameActive)
         {
             gameManager.GameOver();
         }
